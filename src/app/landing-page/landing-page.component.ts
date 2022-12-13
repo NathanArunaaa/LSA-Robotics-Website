9a28d8202f3073f707c054fd524d07a14eba43e6
trulyNathan2 <nathanaruna07@gmail.com>
2022-12-12 20:38:29 -0500

    landing page update

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typed from 'typed.js';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -10,6 +10,23 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const options = {
+      strings: ['LSA R⚙️BOTICS', 'Innovative.', 'Forward Thinking.', 'Perseverance.'],
+      typeSpeed: 90,
+      backSpeed: 90,
+      showCursor: false,
+      fadeOut: false,
+      fadeOutClass: 'typed-fade-out',
+      fadeOutDelay: 1000,
+      loop: true,
+      contentType: 'html',
+ };
+
+ const typed = new Typed('.typed-element', options);
+ 
+
+
+
   }
 
 }
